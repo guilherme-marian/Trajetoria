5db223accca6d27c1b7b41757058c3eadf61f127
Guilherme Marian <guimarian123@gmail.com>
2025-05-21 09:30:23 -0300

    DragCards

--- app.js
+++ app.js
@@ -6,3 +6,39 @@ menu.addEventListener('click', function()
     menu.classList.toggle('is-active')
     menuLinks.classList.toggle('active')
 })
+
+
+let newX = 0, newY = 0, startX = 0, startY = 0
+
+const card = document.getElementById('drag__card')
+
+card.addEventListener('mousedown', mouseDown)
+
+function mouseDown(e)
+{
+    startX = e.clientX
+    startY = e.clientY
+
+    card.addEventListener('mousemove', mouseMove)
+    card.addEventListener('mouseup', mouseUp)
+}
+
+function mouseMove(e)
+{
+    newX = startX - e.clientX
+    newY = startY - e.clientY
+
+    startX = e.clientX
+    startY = e.clientY
+
+    card.style.top = (card.offsetTop - newY) + 'px'
+    card.style.left = (card.offsetLeft - newX) + 'px'
+
+    console.log({newX, newY})
+    console.log({startX, startY})
+}
+
+function mouseUp(e)
+{
+    document.removeEventListener('mousemove', mouseMove)
+}
