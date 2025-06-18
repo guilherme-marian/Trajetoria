152e8ca0ffc449f3285960c0f7e0fc84df75daca
Guilherme Marian <guimarian123@gmail.com>
2025-06-18 10:36:07 -0300

    express

---
+++ src/index.mjs
@@ -0,0 +1,33 @@
+import express, { response } from 'express';
+import mysql from 'mysql2';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+import path from 'path';
+
+const connection = mysql.createConnection ({
+    host: 'localhost',
+    user: 'root',
+    password: 'root'
+})
+
+app.use(express.static('/../'));
+app.use('/../', express.static(__dirname + '/../style.css'));
+
+const PORT = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + "/../index.html") );
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile( + "/notes.html")
+})
+
+app.listen(PORT, () => {
+    console.log(`Running on Port ${PORT}`);
+});
